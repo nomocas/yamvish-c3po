4af6a47abc6a9a3c74e90f686e942b75b86d2d61
Gilles Coomans <gilles.coomans@gmail.com>
2015-11-20 10:36:25 +0100

    work on load for pure context and debug others

--- index.js
+++ index.js
@@ -27,23 +27,20 @@
 		});
 	};
 
-	y.View.prototype.load = y.Template.prototype.load = function(map, arg1, arg2, arg3, arg4) {
-		var path, before, after, fail;
+	y.View.prototype.load = y.Template.prototype.load = function() {
+		var map = arguments[0],
+			argIndex = 1;
 		if (typeof map === 'string') {
-			path = map;
-			map = {};
-			map[path] = arg1;
-			before = arg2;
-			after = arg3;
-			fail = arg4;
-		} else {
-			before = arg1;
-			after = arg2;
-			fail = arg3;
+			map = {}
+			map[arguments[0]] = arguments[1];
+			argIndex = 2;
 		}
-
 		for (var i in map)
 			map[i] = y.interpolable(map[i]);
+
+		var before = arguments[argIndex++],
+			after = arguments[argIndex++],
+			fail = arguments[argIndex++];
 
 		return this.exec(function(context, container) {
 			var self = this;
@@ -67,25 +64,70 @@
 		}, true);
 	};
 
+	y.Context.prototype.load = function() {
+		var map = arguments[0];
+		if (typeof map === 'string') {
+			map = {}
+			map[arguments[0]] = arguments[1];
+		}
+		for (var i in map)
+			map[i] = y.interpolable(map[i]);
+
+		bindMap(map, this, this);
+		var pr = [],
+			uri;
+		for (var i in map) {
+			uri = map[i].__interpolable__ ? map[i].output(this) : map[i];
+			this.setAsync(i, c3po.get(uri));
+		}
+		return this;
+	}
+
 	y.Template.prototype.contentFrom = y.View.prototype.contentFrom = function(uri, before, after, fail) {
 		uri = y.interpolable(uri);
 		return this.exec(function(context, container) {
-			var self = this;
+			var self = this,
+				current, currentURI;
 
-			function applyContent(type, path, uri) {
+			var applyContent = function(type, path, uri) {
+				if (currentURI === uri)
+					return;
+				currentURI = uri;
+				// console.log('contentFrom : update content ', uri, context.data.$route);
+
 				if (before)
 					before.call(self, context, container);
 				return c3po.get(uri).then(function(templ) {
-					if (templ.__yView__)
-						templ.mount(self);
-					else if (templ.call) {
-						y.utils.emptyNode(self);
-						var r = templ.call(self, context, container);
-						if (after && r && r.then)
-							return r.then(function(s) {
+					// console.log('contentFrom : ', uri, ' resolve : ', templ);
+					if (current) {
+						if (current.destroy)
+							current.destroy();
+						else
+							utils.destroyElement(current);
+					}
+					if (templ.__yContainer__)
+						current = templ.mount(self);
+					else if (templ.__yTemplate__) {
+						current = templ.toContainer(context).mount(self);
+						if (after && current && current.then)
+							return current.then(function(s) {
 								return after.call(self, context, container);
 							});
-					} else self.innerHTML = templ;
+					} else {
+						self.innerHTML = templ;
+						current = self.childNodes[0];
+					}
+					if (current._route) {
+						if (container) {
+							container._route = container._route || {
+								subrouters: []
+							};
+							y.router.bindToParentRouter(current, container);
+						} else if (!y.router.bindToParentRouter(current)) // try to bind to parent node that hold a _route entry
+							console.warn('yamvish route has not be attached to parent router. will never fire.');
+					}
+					// console.log('contentFrom : after bind to parent router : ', current._route);
+
 					if (after)
 						return after.call(self, context, container);
 				}, function(e) {
