ace60d9f77bf3975eafc0f528e3afaec702766b5
Gilles Coomans <gilles.coomans@gmail.com>
2016-02-13 11:20:00 +0100

    follow refactoring exec/dom/string/... and comments and dom isolation

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 	y.c3po = c3po;
 
+	//_____________________________________________ CONTEXT LOAD
+
 	function bindMap(map, self, context, before, after, fail) {
 		self._binds = self._binds || [];
 		Object.keys(map).forEach(function(i) {
@@ -27,109 +29,125 @@
 		});
 	};
 
-	y.Template.prototype.load = function() {
-		var map = arguments[0],
-			argIndex = 1;
-		if (typeof map === 'string') {
-			map = {}
-			map[arguments[0]] = arguments[1];
-			argIndex = 2;
-		}
-		for (var i in map)
-			map[i] = y.interpolable(map[i]);
-
-		var before = arguments[argIndex++],
-			after = arguments[argIndex++],
-			fail = arguments[argIndex++];
-
-		return this.exec(function(context, node) {
-			bindMap(map, node, context, before, after, fail);
-			if (before)
-				before.call(node, context);
-			var pr = [],
-				uri;
-			for (var i in map) {
-				uri = map[i].__interpolable__ ? map[i].output(context) : map[i];
-				pr.push(context.setAsync(i, c3po.get(uri)));
-			}
-			return ((pr.length == 1) ? pr[0] : Promise.all(pr)).then(function(s) {
-				if (after)
-					return after.call(node, context);
-			}, function(e) {
-				if (fail)
-					return fail.call(node, context, e);
-				throw e;
-			});
-		}, null, true);
-	};
-
-	y.Context.prototype.load = function() {
-		var map = arguments[0];
-		if (typeof map === 'string') {
-			map = {}
-			map[arguments[0]] = arguments[1];
-		}
-		for (var i in map)
-			map[i] = y.interpolable(map[i]);
-
-		bindMap(map, this, this);
+	function loadData(map, context) {
 		var pr = [],
 			uri;
 		for (var i in map) {
-			uri = map[i].__interpolable__ ? map[i].output(this) : map[i];
-			this.setAsync(i, c3po.get(uri));
+			uri = map[i].__interpolable__ ? map[i].output(context) : map[i];
+			pr.push(context.setAsync(i, c3po.get(uri)));
 		}
+		return ((pr.length == 1) ? pr[0] : Promise.all(pr));
+	};
+
+	y.Context.prototype.load = function() {
+		var map = arguments[0],
+			index = 1;
+		if (typeof map === 'string') {
+			map = {}
+			map[arguments[0]] = arguments[1];
+			index = 2;
+		}
+		for (var i in map)
+			map[i] = y.interpolable(map[i]);
+
+		var before = arguments[index++],
+			after = arguments[index++],
+			fail = arguments[index++];
+
+		bindMap(map, this, this, before, after, fail);
+		if (before)
+			before.call(this, this);
+
+		loadData(map, this).then(function(s) {
+			if (after)
+				after(self, this);
+			return s;
+		}, function(e) {
+			if (fail)
+				fail(self, this, e);
+			throw e;
+		});
 		return this;
+	};
+
+	y.Template.prototype.load = function() {
+		var args = arguments;
+		return this.context(function(context) {
+			context.load.apply(context, args);
+		});
+	};
+
+	//_____________________________________________ Template. CONTENT FROM
+
+	function setContent(promise, after, fail, node, context, closure) {
+		return promise.then(function(templ) {
+			var current = closure ? closure.current : null;
+			if (current) {
+				if (current.destroy)
+					current.destroy();
+				else
+					utils.destroyElement(current);
+			}
+
+			if (templ.__yContainer__) {
+				if (closure) // only node output
+					current = templ.mount(node);
+				else
+					node.children += templ.toString();
+			} else if (templ.__yTemplate__) {
+				if (closure) // only node output
+					current = templ.toContainer(context).mount(node);
+				else
+					node.children += templ.toHTMLString(context);
+			} else {
+				node.innerHTML = templ;
+				current = node.childNodes[0];
+			}
+			if (closure)
+				closure.current = current;
+			if (after)
+				after.call(node, context, container);
+		}, function(e) {
+			console.log('error while loading (contentFrom ) : ', e);
+			if (fail)
+				return fail.call(node, context, container);
+		});
 	}
 
 	y.Template.prototype.contentFrom = function(uri, before, after, fail) {
 		uri = y.interpolable(uri);
-		return this.exec(function(context, node) {
-			var self = this,
-				current, currentURI;
+		var id = new Date().valueOf(); // id for promise retrieval between twopass
+		return this.exec({
+			firstPass: function(context) {
+				context.loadables = context.loadables ||  {};
+				context.loadables[id] = context.waiting(c3po.get(uri));
+			},
+			secondPass: function(context, descriptor) {
+				var promise = context.loadables[id];
+				context.loadables[id] = null;
+				setContent(promise, after, fail, descriptor, context);
+			},
+			dom: function(context, node) {
+				var currentURI, closure = {
+					current: null,
+				};
+				var applyContent = function(uri, type, path) {
+					if (currentURI === uri)
+						return;
+					currentURI = uri;
+					if (before)
+						before.call(node, context, container);
+					context.waiting(setContent(c3po.get(uri), after, fail, node, context, closure));
+				};
 
-			var applyContent = function(uri, type, path) {
-				if (currentURI === uri)
-					return;
-				currentURI = uri;
-
-				if (before)
-					before.call(self, context, container);
-				return c3po.get(uri).then(function(templ) {
-					if (current) {
-						if (current.destroy)
-							current.destroy();
-						else
-							utils.destroyElement(current);
-					}
-					if (templ.__yContainer__)
-						current = templ.mount(self);
-					else if (templ.__yTemplate__) {
-						current = templ.toContainer(context).mount(self);
-						if (after && current && current.then)
-							return current.then(function(s) {
-								return after.call(self, context, container);
-							});
-					} else {
-						self.innerHTML = templ;
-						current = self.childNodes[0];
-					}
-					if (after)
-						return after.call(self, context, container);
-				}, function(e) {
-					console.log('error while loading (contentFrom ) : ', e);
-					if (fail)
-						return fail.call(self, context, container);
-				});
-			};
-
-			if (uri.__interpolable__) {
-				this.binds = this.binds ||  [];
-				uri.subscribeTo(context, applyContent, this.binds);
-				return applyContent(uri.output(context), 'set');
+				if (uri.__interpolable__) {
+					this.binds = this.binds ||  [];
+					uri.subscribeTo(context, applyContent, this.binds);
+					return applyContent(uri.output(context), 'set');
+				}
+				applyContent(uri, 'set');
 			}
-			return applyContent(uri, 'set');
-		}, null, true);
+		});
 	};
 
 	module.exports = c3po;
